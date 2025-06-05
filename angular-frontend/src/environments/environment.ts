30c99c9326954bb1c5981d5c61abe35d56c33073
Alex <alex@Ruths-MacBook-Air.local>
2025-06-06 00:36:25 +0400

    update connection parameters

--- angular-frontend/src/environments/environment.ts
+++ angular-frontend/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5000/api/v1',
+  apiUrl: 'http://localhost:8000/api/v1',
   tokenKey: 'dinner1_auth_token',
-  socketUrl: 'ws://localhost:5000',
+  socketUrl: 'ws://localhost:8000',
   
   // Soul Before Skin configuration
   soulBeforeSkin: {
