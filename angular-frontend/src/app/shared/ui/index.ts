cb3f6cd23e132e26d11b55a391883d98ea42c923
Alexander Sebhat <alexsanderephrem@gmail.com>
2026-04-19 15:42:37 +0400

    feat(frontend): add UI primitives barrel export

---
+++ angular-frontend/src/app/shared/ui/index.ts
@@ -0,0 +1,16 @@
+export { DfButtonDirective } from './button/df-button.directive';
+export type { DfButtonVariant, DfButtonSize } from './button/df-button.directive';
+
+export { DfChipComponent } from './chip/df-chip.component';
+export type { DfChipPurpose } from './chip/df-chip.component';
+
+export { DfCardComponent } from './card/df-card.component';
+export type { DfCardLayout } from './card/df-card.component';
+
+export { DfInputDirective } from './input/df-input.directive';
+
+export { DfAvatarComponent } from './avatar/df-avatar.component';
+export type { DfAvatarSize } from './avatar/df-avatar.component';
+
+export { DfPageShellComponent } from './page-shell/df-page-shell.component';
+export type { DfPageShellVariant } from './page-shell/df-page-shell.component';
